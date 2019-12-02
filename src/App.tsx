9e90dcee6005575e8d31ae5fb625057b8e537061
Hassan Chabab <chababhassan@gmail.com>
2019-12-02 18:57:13 +0100

    add Touchable component

--- src/App.tsx
+++ src/App.tsx
@@ -8,50 +8,27 @@
 
 import * as React from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
-  ScrollView,
   View,
-  Text,
   StatusBar,
 } from 'react-native';
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+import StorybookUIRoot from '../storybook';
 
 const App: React.FC = () => {
   return (
     <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>React Native UI Kit</Text>
-            </View>
-          </View>
-        </ScrollView>
-      </SafeAreaView>
+      <StatusBar barStyle="light-content" />
+      <View style={styles.container}>
+        <StorybookUIRoot />
+      </View>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
+  container: {
+    flex: 1,
   },
 });
 
